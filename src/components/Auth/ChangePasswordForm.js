da58583bac0bc2ea5c88bb5c3f73d234bae2c9ae
長谷川 徹 <tooru.hasegawa@pasnet.local>
2021-07-02 14:19:25 +0900

    allhomework

--- src/components/Auth/ChangePasswordForm.js
+++ src/components/Auth/ChangePasswordForm.js
@@ -1,19 +1,17 @@
 import React, {
     useState,
     useRef,
-    useEffect,
-    useReducer,
     Fragment,
     useContext,
-  } from "react";
+} from "react";
   
-  import axios  from "axios";
-  import Card from "../UI/Card/Card";
-  import classes from "./Auth.module.css";
-  import Button from "../UI/Button/Button";
-  import AuthContext from "../../store/AuthContext";
+import axios  from "axios";
+import Card from "../UI/Card/Card";
+import classes from "./Auth.module.css";
+import Button from "../UI/Button/Button";
+import AuthContext from "../../store/AuthContext";
   
-  const ChangePasswordForm = () => {
+const ChangePasswordForm = () => {
     const [isFormValid, setIsFormValid] = useState(false);
     const [isShowForm, setShowForm] = useState(true);
     const [message, setMessage] = useState(null);
@@ -24,77 +22,71 @@ import React, {
     const passwordInput = useRef();
 
     const checkFomValidHandler= () => {
-      if (
-      passwordInput.current.value === "") {
-        setIsFormValid(false);
-        return;
+        if (
+        passwordInput.current.value === "") {
+            setIsFormValid(false);
+            return;
     }
-      setIsFormValid(true);
+        setIsFormValid(true);
     }
   
     const submitHandler = (e) => {
-      e.preventDefault();
-      if (!isFormValid) {
-        return;
-      }
-      const signupAPI = 'http://localhost:5151/change_password';
-      const signupData = {
-        new_password: passwordInput.current.value,
-      };
-      axios.post('http://localhost:5151/change_password',
-        {
-          "new_password": passwordInput.current.value
-        },{
-      headers: {
-        "Authorization": token
-        }})
-
-      // fetch(signupAPI, {
-      //   method: "POST",
-      //   body: JSON.stringify(signupData),
-      // })
-
-      .then(response => {
-        if (response.status == 200) {
-          setMessage("Password has changed");
-          setShowForm(false);
-          return;
+        e.preventDefault();
+        if (!isFormValid) {
+            return;
         }
-        setMessage("Failed to change password. Please check information!");
-      })
-      .catch((error) => {
-        setMessage(error.message);
-      });
+        axios.post('http://localhost:5151/change_password',
+            {
+                "new_password": passwordInput.current.value
+            },
+            {
+                headers: {
+                    "Authorization": token
+                }
+            }
+        )
+
+        .then(response => {
+            if (response.status === 200) {
+                setMessage("Password has changed");
+                setShowForm(false);
+                return;
+            }
+            setMessage("Failed to change password. Please check information!");
+        })
+        .catch((error) => {
+            setMessage(error.message);
+        });
     };
   
     return (
-      <Fragment>
-        <Card className={classes.auth}>
-          {message != null && <p className={classes.error}>{message}</p>}
-          {isShowForm && (
-            <form onSubmit={submitHandler}>
-              <div className={classes.control}>
-                <label htmlFor="password">New Password</label>
-                <input type="password"
-                id="password"
-                ref={passwordInput}
-                onChange={checkFomValidHandler}
-                />
-              </div>
-              <div className={classes.actions}>
-                <Button
-                  type="submit"
-                  className={classes.btn}
-                  disabled={!isFormValid}
-                >
-                  Change Password
-                </Button>
-              </div>
-            </form>
-          )}
-        </Card>
-      </Fragment>
+        <Fragment>
+            <Card className={classes.auth}>
+                {message != null && <p className={classes.error}>{message}</p>}
+                {isShowForm && (
+                    <form onSubmit={submitHandler}>
+                        <div className={classes.control}>
+                          <label htmlFor="password">New Password</label>
+                          <input type="password"
+                              id="password"
+                              ref={passwordInput}
+                              onChange={checkFomValidHandler}
+                          />
+                        </div>
+                        <div className={classes.actions}>
+                            <Button
+                                type="submit"
+                                className={classes.btn}
+                                disabled={!isFormValid}
+                            >
+                                Change Password
+                            </Button>
+                        </div>
+                    </form>
+                )}
+            </Card>
+        </Fragment>
     );
-  };
+};
   
-  export default ChangePasswordForm;  
+export default ChangePasswordForm;  
